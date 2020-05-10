9d9a111beead2a16b3908e5f325b971c634271a7
Emir <emir.kuanyshev.ktl@gmail.com>
2020-05-10 19:51:31 +0600

    TYP-10: necessary modifications were executed

--- src/components/keyboard/keyboard.tsx
+++ src/components/keyboard/keyboard.tsx
@@ -5,62 +5,12 @@ import { useObservable } from '@commons/react-hooks/use-observable'
 import { isCharacterRequiredShift } from '@commons/characters'
 
 import './keyboard.less';
-import {leftShiftKeys} from "./keyboardCharacteristics";
-import {keyboardKeys} from "./keyboardCharacteristics";
+import { leftShiftKeys, keyboardKeys } from "./keyboardCharacteristics";
+import { Key } from "components/keyboard/key";
 
 type Props = {
     typingAnalyzeState: Observable<TypingAnalyzerState>
 }
-//TODO: ask whether it is correct
-type KeyComponentProps = {
-    character: string | undefined,
-    keyClass: string,
-    keyContent1: string,
-    keyContent2: string|undefined,
-    isShiftActive: boolean|undefined
-}
-
-
-const KeyComponent = (props: KeyComponentProps) => {
-    // TODO: optimize logic
-    const {character, keyClass, keyContent1, keyContent2, isShiftActive} = props;
-
-    if (keyContent1==='shift') {
-        if (keyClass==="left"){
-            if (character && !leftShiftKeys.includes(character)){
-                return (
-                    <div className={`key ${isShiftActive ? 'active' : ''}`}>
-                        <div className={keyClass}>{keyContent1}</div>
-                    </div>
-                )
-            }
-        } else if (keyClass==='right'){
-            if (character && leftShiftKeys.includes(character)){
-                return (
-                    <div className={`key ${isShiftActive ? 'active' : ''}`}>
-                        <div className={keyClass}>{keyContent1}</div>
-                    </div>
-                )
-            }
-        }
-    }
-
-    if (keyContent2===undefined){
-        return (
-            <div className={`key ${character === keyContent1.toLowerCase() || character===keyContent1 ? 'active' : ''}`}>
-                {console.log(character)}
-                <div className={keyClass}>{keyContent1}</div>
-            </div>
-        )
-    }
-    return (
-        <div className={`key ${character === keyContent1 || character === keyContent2 ? 'active' : ''}`}>
-            <div className={keyClass}>{keyContent1}</div>
-            <div className={keyClass}>{keyContent2}</div>
-        </div>
-    )
-}
-
 
 export const Keyboard = (props: Props) => {
     const { typingAnalyzeState } = props
@@ -69,18 +19,34 @@ export const Keyboard = (props: Props) => {
     const isShiftActive = character && isCharacterRequiredShift(character)
 
 
+
     return (
         <div className='keyboard'>
             {keyboardKeys.map((rowKeys,index) => {
                     return (
                         <div key={index+1} className={`row-container row${index + 1}`}>
-                            { rowKeys.map(keyCharacteristics =>
-                                <KeyComponent key={keyCharacteristics.keyContent1} character={character}
-                                              keyClass={keyCharacteristics.keyClass}
-                                              keyContent1={keyCharacteristics.keyContent1}
-                                              keyContent2={keyCharacteristics.keyContent2}
-                                              isShiftActive={isShiftActive}
-                                />
+                            { rowKeys.map(keyCharacteristics => {
+                                const { align, content, minorContent } = keyCharacteristics;
+                                let isActive = character === content || character === content.toLowerCase() || character === minorContent
+                                if (content === 'shift' && isShiftActive){
+                                    if (align === 'left'){
+                                        isActive= character!==undefined && !leftShiftKeys.includes(character)
+                                    } else {
+                                        isActive= character!==undefined && leftShiftKeys.includes(character)
+                                    }
+                                }
+                                return (
+                                    <Key
+                                        key={content}
+                                        align={align}
+                                        isActive={isActive}
+                                        content={content}
+                                        minorContent={minorContent}
+                                    />
+                                )
+
+                            }
+
                             )}
                         </div>
                     )
